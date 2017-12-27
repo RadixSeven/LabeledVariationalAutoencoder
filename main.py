37614965256115abe4c9b017a41dd7201f7fb40d
Eric Moyer <eric_moyer@yahoo.com>
2017-12-27 12:57:14 -0500

    Run labeled autoencoder
    Machine-measured error was greater ... but my human eye thought that the
    mistakes crossed number-boundaries less.

--- main.py
+++ main.py
@@ -13,15 +13,22 @@ class LatentAttention():
         self.n_samples = self.mnist.train.num_examples
 
         self.n_z = 20
+        self.n_labels = 10
         self.batchsize = 100
 
+        # n x 784
         self.images = tf.placeholder(tf.float32, [None, 784])
+        # n x n_labels
+        self.labels = tf.placeholder(tf.float32, [None, self.n_labels])
         image_matrix = tf.reshape(self.images,[-1, 28, 28, 1])
         z_mean, z_stddev = self.recognition(image_matrix)
         samples = tf.random_normal([self.batchsize,self.n_z],0,1,dtype=tf.float32)
+        # n x n_z
         guessed_z = z_mean + (z_stddev * samples)
+        # n x n_z + n_labels
+        labeled_z = tf.concat([self.labels, guessed_z], 1)
 
-        self.generated_images = self.generation(guessed_z)
+        self.generated_images = self.generation(labeled_z)
         generated_flat = tf.reshape(self.generated_images, [self.batchsize, 28*28])
 
         self.generation_loss = -tf.reduce_sum(self.images * tf.log(1e-8 + generated_flat) + (1-self.images) * tf.log(1e-8 + 1 - generated_flat),1)
@@ -32,6 +39,8 @@ class LatentAttention():
 
 
     # encoder
+    # Takes n x 28 x 28 x 1 image tensor
+    # outputs two batchsize x n_z tensors ... mean, standard dev
     def recognition(self, input_images):
         with tf.variable_scope("recognition"):
             h1 = lrelu(conv2d(input_images, 1, 16, "d_h1")) # 28x28x1 -> 14x14x16
@@ -46,7 +55,7 @@ class LatentAttention():
     # decoder
     def generation(self, z):
         with tf.variable_scope("generation"):
-            z_develop = dense(z, self.n_z, 7*7*32, scope='z_matrix')
+            z_develop = dense(z, self.n_z+self.n_labels, 7*7*32, scope='z_matrix')
             z_matrix = tf.nn.relu(tf.reshape(z_develop, [self.batchsize, 7, 7, 32]))
             h1 = tf.nn.relu(conv_transpose(z_matrix, [self.batchsize, 14, 14, 16], "g_h1"))
             h2 = conv_transpose(h1, [self.batchsize, 28, 28, 1], "g_h2")
@@ -55,22 +64,27 @@ class LatentAttention():
         return h2
 
     def train(self):
-        visualization = self.mnist.train.next_batch(self.batchsize)[0]
-        reshaped_vis = visualization.reshape(self.batchsize,28,28)
-        ims("results/base.jpg",merge(reshaped_vis[:64],[8,8]))
+        visualization_im, visualization_lab = self.mnist.train.next_batch(self.batchsize)
+        reshaped_vis_im = visualization_im.reshape(self.batchsize,28,28)
+        ims("results/base.jpg",merge(reshaped_vis_im[:64],[8,8]))
         # train
         saver = tf.train.Saver(max_to_keep=2)
         with tf.Session() as sess:
             sess.run(tf.initialize_all_variables())
-            for epoch in range(2):
+            for epoch in range(15):
                 for idx in range(int(self.n_samples / self.batchsize)):
-                    batch = self.mnist.train.next_batch(self.batchsize)[0]
-                    _, gen_loss, lat_loss = sess.run((self.optimizer, self.generation_loss, self.latent_loss), feed_dict={self.images: batch})
+                    batch_images, batch_labels = self.mnist.train.next_batch(
+                        self.batchsize)
+                    _, gen_loss, lat_loss = sess.run(
+                        (self.optimizer, self.generation_loss,
+                         self.latent_loss), feed_dict={
+                             self.images: batch_images,
+                             self.labels: batch_labels})
                     # dumb hack to print cost every epoch
                     if idx % (self.n_samples - 3) == 0:
                         print("epoch {}: genloss {} latloss {}".format(epoch, np.mean(gen_loss), np.mean(lat_loss)))
                         saver.save(sess, os.getcwd()+"/training/train",global_step=epoch)
-                        generated_test = sess.run(self.generated_images, feed_dict={self.images: visualization})
+                        generated_test = sess.run(self.generated_images, feed_dict={self.images: visualization_im, self.labels: visualization_lab})
                         generated_test = generated_test.reshape(self.batchsize,28,28)
                         ims("results/"+str(epoch)+".jpg",merge(generated_test[:64],[8,8]))
 
