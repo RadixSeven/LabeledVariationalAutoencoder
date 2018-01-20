4f57cffd1c32f213615df1cedcdf00078821d720
Eric Moyer <eric_moyer@yahoo.com>
2018-01-19 21:54:20 -0500

    Reformat for PEP8

--- no_labels.py
+++ no_labels.py
@@ -1,12 +1,12 @@
 import tensorflow as tf
 import numpy as np
 import tensorflow.examples.tutorials.mnist.input_data as input_data
-import matplotlib.pyplot as plt
 import os
 from scipy.misc import imsave as ims
-from utils import *
-from ops import *
+from utils import merge
+from ops import conv2d, conv_transpose, dense, lrelu
 from next_batch_partial import next_batch_partial
+
 
 class LatentAttention():
     def __init__(self, frac_train, n_z, batchsize, learning_rate):
@@ -30,41 +30,46 @@ class LatentAttention():
         self.batchsize = batchsize
 
         self.images = tf.placeholder(tf.float32, [None, 784])
-        image_matrix = tf.reshape(self.images,[-1, 28, 28, 1])
-        z_mean, z_stddev = self.recognition(image_matrix)
-        samples = tf.random_normal(tf.shape(z_stddev),0,1,dtype=tf.float32)
+        image_matrix = tf.reshape(self.images, [-1, 28, 28, 1])
+        z_mean, z_stddev = self.encode(image_matrix)
+        samples = tf.random_normal(tf.shape(z_stddev), 0, 1, dtype=tf.float32)
         guessed_z = z_mean + (z_stddev * samples)
 
-        self.generate_images = self.generation(guessed_z)
+        self.generate_images = self.decode(guessed_z)
         generated_flat = tf.reshape(self.generate_images, [-1, 28*28])
 
-        self.calc_generation_loss = -tf.reduce_sum(self.images * tf.log(1e-8 + generated_flat) + (1-self.images) * tf.log(1e-8 + 1 - generated_flat),1)
+        self.calc_generation_loss = -tf.reduce_sum(
+            self.images * tf.log(1e-8 + generated_flat)
+            + (1-self.images) * tf.log(1e-8 + 1 - generated_flat), 1)
 
-        self.calc_latent_loss = 0.5 * tf.reduce_sum(tf.square(z_mean) + tf.square(z_stddev) - tf.log(tf.square(z_stddev)) - 1,1)
-        self.cost = tf.reduce_mean(self.calc_generation_loss + self.calc_latent_loss)
+        self.calc_latent_loss = 0.5 * tf.reduce_sum(
+            tf.square(z_mean) + tf.square(z_stddev) -
+            tf.log(tf.square(z_stddev)) - 1, 1)
+        self.cost = tf.reduce_mean(
+            self.calc_generation_loss + self.calc_latent_loss)
         self.optimizer = tf.train.AdamOptimizer(
             learning_rate).minimize(self.cost)
 
-
-    # encoder
-    def recognition(self, input_images):
-        with tf.variable_scope("recognition"):
-            h1 = lrelu(conv2d(input_images, 1, 16, "d_h1")) # 28x28x1 -> 14x14x16
-            h2 = lrelu(conv2d(h1, 16, 32, "d_h2")) # 14x14x16 -> 7x7x32
-            h2_flat = tf.reshape(h2,[-1, 7*7*32])
+    def encode(self, input_images):
+        with tf.variable_scope("encode"):
+            # 28x28x1 -> 14x14x16
+            h1 = lrelu(conv2d(input_images, 1, 16, "d_h1"))
+            # 14x14x16 -> 7x7x32
+            h2 = lrelu(conv2d(h1, 16, 32, "d_h2"))
+            h2_flat = tf.reshape(h2, [-1, 7*7*32])
 
             w_mean = dense(h2_flat, 7*7*32, self.n_z, "w_mean")
             w_stddev = dense(h2_flat, 7*7*32, self.n_z, "w_stddev")
 
         return w_mean, w_stddev
 
-    # decoder
-    def generation(self, z):
-        with tf.variable_scope("generation"):
+    def decode(self, z):
+        with tf.variable_scope("decode"):
             z_shape = tf.shape(z)
             z_develop = dense(z, self.n_z, 7*7*32, scope='z_matrix')
             z_matrix = tf.nn.relu(tf.reshape(z_develop, [-1, 7, 7, 32]))
-            h1 = tf.nn.relu(conv_transpose(z_matrix, [z_shape[0], 14, 14, 16], "g_h1"))
+            h1 = tf.nn.relu(
+                conv_transpose(z_matrix, [z_shape[0], 14, 14, 16], "g_h1"))
             h2 = conv_transpose(h1, [z_shape[0], 28, 28, 1], "g_h2")
             h2 = tf.nn.sigmoid(h2)
 
@@ -83,7 +88,8 @@ class LatentAttention():
 
         print("epoch {:02d}: genloss {:7.3f} latloss {:7.3f} "
               "validation_genloss {:7.3f}".format(
-            epoch, np.mean(gen_loss), np.mean(lat_loss), np.mean(val_error)))
+                  epoch,
+                  np.mean(gen_loss), np.mean(lat_loss), np.mean(val_error)))
 
     def train(self):
         data = self.mnist.train
@@ -94,8 +100,8 @@ class LatentAttention():
             validation = data.images[self.n_train:]
             val_labels = data.labels[self.n_train:]
 
-        reshaped_val = validation.reshape(-1,28,28)
-        ims("results/base.jpg", merge(reshaped_val[:64],[8,8]))
+        reshaped_val = validation.reshape(-1, 28, 28)
+        ims("results/base.jpg", merge(reshaped_val[:64], [8, 8]))
         # train
         saver = tf.train.Saver(max_to_keep=2)
         with tf.Session() as sess:
